a6e931b23b750cac693b0adf0a67858ea4b1165b
Jacobschoenbauer <jhwt65@gmail.com>
2022-12-11 13:24:45 -0600

    middleware  problem

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const useroutes = require("./useroutes");
 const blogRouter = require("./blogroutes")
+
+
 router.use("/user", useroutes);
 router.use("/blog", blogRouter)
 
